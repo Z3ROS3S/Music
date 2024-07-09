a24761b34d710620fe88d5bd0c72e9b2334504ce
My name is Z <88902026+Z3ROS3S@users.noreply.github.com>
2024-07-09 10:47:17 -0700

    Update __main__.py
    Signed-off-by: My name is Z <88902026+Z3ROS3S@users.noreply.github.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -3,6 +3,7 @@ import importlib
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
+from pyrogram.errors import FloodWait
 
 import config
 from AnonXMusic import LOGGER, app, userbot
@@ -33,7 +34,12 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-    await app.start()
+    try:
+        await app.start()
+    except FloodWait as e:
+        print(f"FloodWait: Harus menunggu {e.x} detik.")
+        await asyncio.sleep(e.x)
+        await init()  # Coba lagi setelah menunggu
     for all_module in ALL_MODULES:
         importlib.import_module("AnonXMusic.plugins" + all_module)
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
@@ -50,7 +56,7 @@ async def init():
         pass
     await Anony.decorators()
     LOGGER("AnonXMusic").info(
-        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
+        "Ilumi Player Bot Started Successfully.\n\nDon't forget to visit @"
     )
     await idle()
     await app.stop()
